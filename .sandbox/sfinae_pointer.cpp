63770278a3964b5e9c0bd6962d0e9ee8b6a5bd61
yuki <shimazuyuki7@gmail.com>
2022-10-04 17:26:03 +0900

    clean

--- .sandbox/sfinae_pointer.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-using namespace std;
-
-template <class T>
-typename remove_reference<T>::type&& move(T&& t) {
-  typedef typename remove_reference<T>::type U;
-  return static_cast<U&&>(t);
-}
